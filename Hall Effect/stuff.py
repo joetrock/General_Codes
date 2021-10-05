5b5c567a854d6cae3b70aae1fc8ff74579f64656
joetrock <joesantrock@gmail.com>
2021-10-05 01:13:42 -0400

    Hall Effect added

---
+++ Hall Effect/stuff.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Axial = (-7.7,-8.28,-7.75,-15,16.14,-6.49,-30.62)
+Perpen = (-0.01,-0.01,-0.01,-0.01,-0.000701,-0.000723,-0.000762)
+Horizontal = (15.5,15.5,16,6,10.4,18.8,18.8)
+Rail = (9.5,12.3,9.5,13.1,15.1,20.7,14)
+
+plt.plot(Rail,Perpen,".")
+plt.show()
